15c44244215ea33b06d687e47d9820bd1e5b6aa5
Naoki Kishida <kishida@linecorp.com>
2019-01-04 18:21:02 +0900

    start to implement truffle function

--- src/main/java/kis/phpparser/truffle/PHPControlFlow.java
+++ src/main/java/kis/phpparser/truffle/PHPControlFlow.java
@@ -2,7 +2,6 @@ package kis.phpparser.truffle;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.nodes.ControlFlowException;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.nodes.Node.Child;
 import com.oracle.truffle.api.nodes.NodeInfo;
@@ -14,30 +13,12 @@ import java.util.List;
 import kis.phpparser.truffle.TrufllePHPNodes.PHPExpression;
 import kis.phpparser.truffle.TrufllePHPNodes.PHPStatement;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 /**
  *
  * @author naoki
  */
 public class PHPControlFlow {
-    @AllArgsConstructor @Getter
-    static class ReturnException extends ControlFlowException {
-        Object result;
-    }
-    
-    @NodeInfo(shortName = "return")
-    @AllArgsConstructor
-    static class PHPReturnNode extends PHPStatement {
-        @Child private PHPExpression result;
-
-        @Override
-        void executeVoid(VirtualFrame virtualFrame) {
-            Object value = result.executeGeneric(virtualFrame);
-            throw new ReturnException(value);
-        }
-    }
-    
     @NodeInfo(shortName = "if")
     @AllArgsConstructor
     static class PHPIfNode extends PHPStatement {
