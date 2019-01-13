01975306984dd075746059c4a40b661c13375519
kishida <naokikishida@gmail.com>
2019-01-14 06:19:59 +0900

    Invoke with sample file

--- language/src/main/java/kis/phpparser/truffle/PHPMain.java
+++ language/src/main/java/kis/phpparser/truffle/PHPMain.java
@@ -1,42 +1,16 @@
 package kis.phpparser.truffle;
 
+import java.io.File;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
 
 /**
  *
  * @author naoki
  */
 public class PHPMain {
-    private static final String SCRIPT = ""
-            + "<?php "
-            + "function f($b) {"
-            + "  return 4;"
-            + "}"
-            + "$a = 3;"
-            + "echo f(3) - 3 < 5;"
-            + "echo $a;";
-    public static final String FIB = "" +
-            "<?php " +
-            "function fib($n) {\n" +
-            "    if ($n < 2) {\n" +
-            "        return $n;\n" +
-            "    }\n" +
-            "    return fib($n - 1) + fib($n - 2);\n" +
-            "}\n" +
-            "$start = microtime(1);\n" +
-            "echo \"fib:\".fib(31);\n" +
-            "echo \"\\n\";\n" +
-            "echo \"time:\".(microtime(1) - $start);\n" +
-            "echo \"\\n\";\n" +
-            "fib(31);fib(31);fib(31);fib(31);fib(31);" +
-            "$start = microtime(1);\n" +
-            "echo \"fib:\".fib(31);\n" +
-            "echo \"\\n\";\n" +
-            "echo \"time:\".(microtime(1) - $start);\n" +
-            "echo \"\\n\";\n" +
-            "";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Context ctx = Context.create("php");
-        ctx.eval("php", FIB);
+        ctx.eval(Source.newBuilder("php", new File("../sample/smallfib.php")).build());
     }
 }
